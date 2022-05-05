55bb7844bced7b69ac577b906784f4bfeb97a497
Ryderxz4224 <62250913+Ryderxz4224@users.noreply.github.com>
2022-05-05 13:28:41 +0300

    Doua tickete. BAM. APi calluri pentru a afla vremea la locatia curenta bazandu-te pe IP

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,37 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
+
+  const _ = require("lodash");
+  let url = 'https://ipapi.co/2a02:2f04:c40d:6100:1155:c13b:a5db:73bc/json/';
+  let lat = 0
+  let long = 0
+  let weather = "poateploua"
+  let vreme = "mayberain"
+  fetch(url)
+    .then(res => res.json())
+      .then(out => {
+        lat = _.get(out, "latitude", null)
+        long = _.get(out, "longitude", null)
+      })
+        .catch(err => error);
+  
+  url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=5667d20d425ea4533bdbc1d9e3ec3962"
+  fetch(url)
+    .then(res => res.json())
+      .then(out => {
+        console.log(out)
+        weather = _.get(out, "weather", null)
+        console.log(weather)
+        vreme = _.get(weather[0], "main", null)
+        console.log(vreme)
+      })
+        .catch(err => error);
+
   return (
+    
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -17,7 +46,9 @@ export default function Home() {
         </h1>
 
         <p className={styles.oprisan}>
-          Da!
+          {vreme === "Rain" && "Da!"}
+          {vreme != "Extreme" && "AOLEEEEEEEU!"}
+          {vreme != "Rain" && "Nu!"}
         </p>
 
       </main>
